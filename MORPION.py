fa386a12e33162a3108c0af648c0ce8dfe35a3ec
oualid ahamri <oualid-ahamri@laplateforme.io>
2024-12-03 16:18:48 +0100

    modification

--- MORPION.py
+++ MORPION.py
@@ -1,16 +1,14 @@
 # affichage de la grille de morpion
+grille = [' '] * 9
+print(grille)
 
-grille = ["","","","","","","","",""]
-
-joueur_actuel = "x","O"
+# structuré la grille de morpion
 
 def affichage_grille(grille):
-    print(grille)
-    print(grille[1] + "|" + grille[2] + "|" grille[3])
-    print("--------------")
-    print(grille[4] + "|" + grille[5] + "|" grille[6])
-    print("--------------")
-    print(grille[7] + "|" + grille[8] + "|" grille[9])
-    print("--------------")
-    print(grille)
-# TEST
+    print("""          |        |    
+      -------------------
+          |        | 
+      -------------------
+          |        | 
+      """)
+affichage_grille(grille)
