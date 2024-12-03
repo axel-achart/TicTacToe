af8e592a17520e98c5b8b53b9535c798d191e0ce
oualid ahamri <oualid-ahamri@laplateforme.io>
2024-12-03 10:23:47 +0100

    Ajout du tableau verge premiere version

---
+++ MORPION.py
@@ -0,0 +1,15 @@
+# affichage de la grille de morpion
+
+grille = ["","","","","","","","",""]
+
+joueur_actuel = "x","O"
+
+def affichage_grille(grille):
+    print(grille)
+    print(grille[1] + "|" + grille[2] + "|" grille[3])
+    print("--------------")
+    print(grille[4] + "|" + grille[5] + "|" grille[6])
+    print("--------------")
+    print(grille[7] + "|" + grille[8] + "|" grille[9])
+    print("--------------")
+    print(grille)
